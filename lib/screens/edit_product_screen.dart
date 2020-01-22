c1a3dc2befb85dec213bfd409b17c80400f6ecc9
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-22 20:47:09 +0100

    multiline inputs, disposing objects

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
-  
+
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  // we need to dispose of FocusNodes so that we dont have memory leaks
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +47,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                ),
+                maxLines: 3,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
               ),
             ],
           ),
