98ec5a74a72e830bba4efd35c91f1d815400200b
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-22 14:31:13 +0100

    adding multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,29 @@ import 'package:provider/provider.dart'; // required for provider
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart'
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // with .value is the right approach if we use provider on something thats part of a list or a grid
-    // so that now we can make sure Provider works even if we change data in the widget and with builder 
-    // function it will not work correctly
-    return ChangeNotifierProvider.value(
-      // providing Products() to all child widgets of MaterialApp, so now all child widgets can set
-      // up a listener that will trigger a rebuild only for listeners
-      value: Products(),
-      // builder: (context) => Products(),
+    // adding multiple proviuders
+    return MultiProvider(
+      providers: [
+        // with .value is the right approach if we use provider on something thats part of a list or a grid
+        // so that now we can make sure Provider works even if we change data in the widget and with builder
+        // function it will not work correctly
+        // providing Products() to all child widgets of MaterialApp, so now all child widgets can set
+        // up a listener that will trigger a rebuild only for listeners
+        // builder: (context) => Products(),
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'My Shop',
         theme: ThemeData(
