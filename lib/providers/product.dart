ed7e97452408b1e2947ed38db71e1834483136cb
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-23 19:24:04 +0100

    updating favorite status

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
-class Product with ChangeNotifier{
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -17,8 +19,30 @@ class Product with ChangeNotifier{
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  void _setFavoriteValue(bool value) {
+    isFavorite = value;
     notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+
+    _setFavoriteValue(!isFavorite);
+
+    // Optimistic updating
+    final url = 'https://flutter-shop-app-6fa69.firebaseio.com/products/$id.json';
+
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          }));
+
+      if (response.statusCode >= 400) {
+        _setFavoriteValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavoriteValue(oldStatus);
+    }
   }
 }
