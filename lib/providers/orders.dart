883dec6bb547f1e27d88a415e1e895fcbdd9fbef
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-23 20:37:37 +0100

    storing orders in api

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import './cart.dart';
 
@@ -24,15 +26,32 @@ class Orders with ChangeNotifier {
   }
 
   // add whole cart to order
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://flutter-shop-app-6fa69.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts
+              .map((cartProduct) => {
+                    'id': cartProduct.id,
+                    'title': cartProduct.title,
+                    'quantity': cartProduct.quantity,
+                    'price': cartProduct.price,
+                  })
+              .toList(),
+        }));
+
     _orders.insert(
         0,
         OrderItem(
-          id: DateTime.now().toString(),
+          id: json.decode(response.body)['id'],
           amount: total,
-          dateTime: DateTime.now(),
+          dateTime: timestamp,
           products: cartProducts,
         ));
-        notifyListeners();
+    notifyListeners();
   }
 }
--- lib/screens/cart_screens.dart
+++ lib/screens/cart_screens.dart
@@ -41,15 +41,7 @@ class CartScreen extends StatelessWidget {
                       ),
                       backgroundColor: Theme.of(context).primaryColor,
                     ),
-                    FlatButton(
-                      child: Text('ORDER NOW'),
-                      onPressed: () {
-                        Provider.of<Orders>(context, listen: false).addOrder(
-                            cart.items.values.toList(), cart.totalAmount);
-                            cart.clear();
-                      },
-                      textColor: Theme.of(context).primaryColor,
-                    )
+                    OrderButton(cart: cart)
                   ],
                 ),
               ),
@@ -70,5 +62,42 @@ class CartScreen extends StatelessWidget {
             ),
           ],
         ));
+  }
+}
+
+// moved it so we dont have to change state on the whole tree in CartScreen
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderButtonState createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: _isLoading ? CircularProgressIndicator() : Text('ORDER NOW'),
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null // Button is automatically disabled
+          : () async {
+              setState(() {
+                _isLoading = true;
+              });
+              await Provider.of<Orders>(context, listen: false).addOrder(
+                  widget.cart.items.values.toList(), widget.cart.totalAmount);
+              setState(() {
+                _isLoading = false;
+              });
+              widget.cart.clear();
+            },
+      textColor: Theme.of(context).primaryColor,
+    );
   }
 }
