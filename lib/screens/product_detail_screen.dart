94697af7a98805826e12ef3df76bc724528ff268
Ivan Krsnik <ivankrsniksa@gmail.com>
2020-01-25 12:33:37 +0100

    Hero between widgets fade in

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -26,9 +26,13 @@ class ProductDetailScreen extends StatelessWidget {
             Container(
               height: 300,
               width: double.infinity,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
+              // Hero alowes us to fade in widget between screeens
+              child: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             SizedBox(
